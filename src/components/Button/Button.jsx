a1548035f02076ebc85de57e84118851450aa1f8
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-03-21 21:12:23 +0200

    icons

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { FaMobileScreenButton } from "react-icons/fa6";
+import s from "./Button.module.css";
 
 const Button = ({ children }) => {
-  return <button>{children}</button>;
+  return (
+    <button>
+      <FaMobileScreenButton className={s.icon} /> {children}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,14 +1,21 @@
 import s from "./Message.module.css";
+import clsx from "clsx";
 
 const Message = ({ author = "Incognito", text, isOnline = false }) => {
   return (
     <div>
       <h3 className={s.title}>{author}</h3>
-      {isOnline ? <h6>Online</h6> : <h6>Offline</h6>}
-      {isOnline && <h4>Написати</h4>}
+      {isOnline ? (
+        <h6 className={clsx(s.status, s.green)}>Online</h6>
+      ) : (
+        <h6 className={clsx(s.status, s.red)}>Offline</h6>
+      )}
+      {isOnline && <h4>Write</h4>}
 
+      {/* <h6 className={`${s.status} ${s.green}`}>Online</h6>
+      <h6 className={`${s.status} ${s.red}`}>Offline</h6> */}
       {/* <h7>Hello World</h7> */}
-      <p>{text}</p>
+      <p className={clsx(text.length > 10 && s.alert)}>{text}</p>
     </div>
   );
 };
